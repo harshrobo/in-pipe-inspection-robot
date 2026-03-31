5f9f14667fc815ca97080f438ce47482f1322f3d
Harsh Sharma <07harshsharma2001@gmail.com>
2026-03-31 07:57:31 +0530

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,45 @@
+// In-Pipe Robot Control Code
 
+int motorA1 = 5;
+int motorA2 = 6;
+int vibMotor = 9;
+
+void setup() {
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(vibMotor, OUTPUT);
+}
+
+// Expand chassis
+void expand() {
+  digitalWrite(motorA1, HIGH);
+  digitalWrite(motorA2, LOW);
+}
+
+// Contract chassis
+void contract() {
+  digitalWrite(motorA1, LOW);
+  digitalWrite(motorA2, HIGH);
+}
+
+// Stop motor
+void stopMotor() {
+  digitalWrite(motorA1, LOW);
+  digitalWrite(motorA2, LOW);
+}
+
+// Move forward using vibration motor
+void moveForward() {
+  digitalWrite(vibMotor, HIGH);
+}
+
+void loop() {
+  expand();
+  delay(3000);
+
+  stopMotor();
+  delay(1000);
+
+  moveForward();
+  delay(5000);
+}
